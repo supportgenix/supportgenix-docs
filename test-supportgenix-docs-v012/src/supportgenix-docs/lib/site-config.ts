aaf7ccf3535f1af7d2800628dd54651c454804bc
Hasib <aslamhasib99@gmail.com>
2026-05-28 10:27:32 +0600

    fix: handle empty docs install and host favicon assumptions

---
+++ test-supportgenix-docs-v012/src/supportgenix-docs/lib/site-config.ts
@@ -0,0 +1,17 @@
+export const SITE_CONFIG = {
+    brandName: "SupportGenix Docs",
+    organizationName: "Your Company",
+    defaultAuthor: "Documentation Team",
+    siteUrl: "https://example.com",
+    links: {
+        home: "/",
+        docsHome: "/docs",
+        blog: "#",
+        contact: "#",
+        about: "#",
+        terms: "#",
+        privacy: "#",
+        twitter: "#",
+        github: "#",
+    },
+} as const;
